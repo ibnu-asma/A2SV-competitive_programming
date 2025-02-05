899bf88a99f1ecdd7eef88d3178bd80bfeaaf97c
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-05 23:56:02 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 245222.py
@@ -0,0 +1,10 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+n = int(input())
+accepted_from = list(map(int, input().split()))
+
+result = [0] * n 
+for i in range(len(accepted_from)):
+    result[accepted_from[i] - 1] = i + 1
+
+print(*result)

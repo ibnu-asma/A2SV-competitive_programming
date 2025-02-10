316fdf7c582ad46f40c65b28efcfe884bd090394
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-10 10:27:30 +0300

    Created 'Dragons' by A2SV Hub

---
+++ 10-Feb-2025/Dragons 252876.py
@@ -0,0 +1,27 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+def solve(s:int, n: int) -> str:
+    dragons = []
+    for _ in range(n):
+        strength ,  bouns = map(int, input().split())
+        dragons.append([strength, bouns])
+
+    dragons =  sorted(dragons,key= lambda x:x[0])
+    is_winner = True
+    for daragon in dragons:
+        if s > daragon[0]:
+            s += daragon[1]
+        else:
+            is_winner = False
+            break
+    if is_winner:
+        return "YES"
+    return "NO"
+    
+
+if __name__ == '__main__':
+    s, n = map(int, input().split())
+    answer = solve(s, n)
+    print(answer)
+
+    

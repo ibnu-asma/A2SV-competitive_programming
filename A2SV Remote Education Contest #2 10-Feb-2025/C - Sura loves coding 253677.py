1c796a0ec16d6a6ce9a3604dc4b0df46c84b7e64
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-10 16:21:17 +0300

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/C - Sura loves coding 253677.py
@@ -0,0 +1,20 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+
+def solve(n:int, encoded:str):
+    original = []
+    for letter in reversed(encoded):
+        length = len(original)
+        pos = length // 2
+        original.insert(pos, letter)
+    return ''.join(original)
+
+
+                 
+                 
+if __name__ == '__main__':
+    n = int(input())
+    encoded_word = input()
+    decoded = solve(n, encoded_word)
+    print(decoded)
+  

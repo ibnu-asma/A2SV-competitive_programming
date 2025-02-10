2013ac580641e4a8ca5b0aaa5a29f69e15686d23
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-10 16:18:37 +0300

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/D - Mugisho and Rufeyda 253671.py
@@ -0,0 +1,16 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+def solve(n:int, t:int):
+    number  = int("9" * n)
+    min_number = 10** (n -1)
+    while number >= min_number:
+        if number % t == 0:
+            return number
+        number -= 1
+    return -1
+            
+    
+if __name__ == '__main__':
+    n, t = map(int, input().split())
+    result = solve(n, t)
+    print(result)

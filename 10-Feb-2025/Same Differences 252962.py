075d9795ba1db87123302996eb49f38f32612125
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-10 11:13:59 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 10-Feb-2025/Same Differences 252962.py
@@ -0,0 +1,21 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+def solve(n:int):
+    for _ in range(n):
+        counts = {}
+        count = 0
+        length, array = int(input()), list(map(int, input().split()))
+        for i, element in enumerate(array):
+            diff = element - i
+            if diff in counts:
+                count += counts[diff]
+            counts[diff] = counts.get(diff, 0) + 1
+            # count = counts[diff] - 1
+        # print(counts)
+        print(count)
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    solve(n)

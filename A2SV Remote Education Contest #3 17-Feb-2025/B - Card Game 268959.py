6a8a908908766d4268d3284d7b02a06992c679e1
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-17 14:59:43 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/B - Card Game 268959.py
@@ -0,0 +1,26 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+n = int(input())
+players = n // 2
+sequences = list(map(int, input().split()))
+target = sum(sequences) // players
+is_paired = [False]*n
+
+
+for i, seq in enumerate(sequences):
+    if is_paired[i]:
+        continue
+    for j in range(i + 1, n):
+        if not is_paired[j] and sequences[j] + sequences[i] == target:
+            print(i + 1, j + 1)
+            is_paired[i] = True
+            is_paired[j] = True
+            break
+        
+
+    
+    
+
+    
+
+

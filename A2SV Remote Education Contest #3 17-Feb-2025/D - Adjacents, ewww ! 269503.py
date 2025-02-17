891beb4574874d23166212ac719a1bb36715015f
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-17 20:45:10 +0300

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/D - Adjacents, ewww ! 269503.py
@@ -0,0 +1,27 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    matrx_size = int(input())
+    if matrx_size == 2:
+        print(-1)
+    else:
+        items = [t for t in range(1, matrx_size**2 + 1)]
+        count = 0
+        for i in range(0,len(items), 2):
+            count += 1
+            print(items[i], " ", end="")
+            if i > 0 and count % matrx_size == 0:
+                print()
+            if len(items) == 1:
+                print()
+        for i in range(1,len(items), 2):
+            print(items[i], " ", end= "")
+            count += 1
+            if count % matrx_size == 0:
+                print()
+
+
+
+    

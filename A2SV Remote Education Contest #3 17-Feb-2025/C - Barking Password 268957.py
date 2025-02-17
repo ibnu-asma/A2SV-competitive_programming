15d9bec58fa22e6c22048efee4e8f053ad56eefe
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-17 14:59:36 +0300

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 268957.py
@@ -0,0 +1,20 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+password = input()
+n = int(input())
+words = [input().strip() for _ in range(n)]
+
+if password in words:
+    print("YES")
+    exit()
+is_done = False
+for i in range(n):
+    for j in range(n):
+         if words[i][1] + words[j][0] == password or words[j][1] + words[i][0] == password:
+            is_done = True
+            print('YES')
+            break
+    if is_done:
+        break
+if not is_done:
+    print('NO')

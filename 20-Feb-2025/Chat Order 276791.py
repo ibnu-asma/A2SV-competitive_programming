36b600730175178994672f0fcef997108b74ad11
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-20 17:46:53 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 20-Feb-2025/Chat Order 276791.py
@@ -0,0 +1,12 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+friends = [input() for _ in range(n)]
+has_conversation = set()
+check_list = []
+for friend in reversed(friends):
+    if friend in has_conversation:
+        continue
+    print(friend)
+    has_conversation.add(friend)
+

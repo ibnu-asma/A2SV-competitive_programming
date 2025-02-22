3dd39e812a92eee1ec1fffc7b343f1b47829986e
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-22 19:17:41 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 22-Feb-2025/A. Beautiful Matrix 280348.py
@@ -0,0 +1,14 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+x = 0
+y = 0
+for _ in range(5):
+    current_list = list(map(int, input().split()))
+    if 1 in current_list:
+        y = current_list.index(1)
+        break
+    x += 1
+
+print(abs(2 - x ) + abs(2 - y))
+
+

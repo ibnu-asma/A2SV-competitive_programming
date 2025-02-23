93ee2583e1802a1c4252afbe05672ba8ccade1d3
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-23 16:56:16 +0300

    Created 'D - Great Photographer' by A2SV Hub

---
+++ A2SV Remote Contest #4 23-Feb-2025/D - Great Photographer 281117.py
@@ -0,0 +1,25 @@
+# Problem: D - Great Photographer - https://codeforces.com/gym/590201/problem/D
+
+n , x0 = map(int, input().split())
+segments = []
+for _ in range(n):
+    ai, bi = map(int, input().split())
+    segments.append((ai, bi))
+left = 0
+right = 1000
+for ai, bi in segments:
+    left_segment = min(ai, bi)
+    right_segment = max(ai, bi)
+    left = max(left, left_segment)
+    right = min(right, right_segment)
+
+if left > right:
+    print(-1)
+else: 
+    if x0 < left:
+        distance = left - x0
+    elif x0 > right:
+        distance = x0 - right
+    else:
+        distance = 0
+    print(distance)

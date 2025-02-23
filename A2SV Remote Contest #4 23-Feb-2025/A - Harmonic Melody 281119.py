9db73c187d7a7756b21c27df0ef9f9ecd79ab04d
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-23 16:57:14 +0300

    Created 'A - Harmonic Melody' by A2SV Hub

---
+++ A2SV Remote Contest #4 23-Feb-2025/A - Harmonic Melody 281119.py
@@ -0,0 +1,17 @@
+# Problem: A - Harmonic Melody - https://codeforces.com/gym/590201/problem/A
+
+melodies = int(input())
+for _ in range(melodies):
+    notes_num = int(input())
+    notes = list(map(int, input().split()))
+    is_perfect = False
+    for i in range(len(notes) - 1):
+        diff = abs(notes[i] - notes[i + 1])
+        if diff == 5 or diff == 7:
+            continue
+        is_perfect = True
+        print("NO")
+        break
+    if not is_perfect:
+        print('YES')
+

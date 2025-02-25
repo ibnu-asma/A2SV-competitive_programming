d74a53cca79c147e14f73f2649aba11fdfb7be2a
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-25 09:09:35 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 25-Feb-2025/Fox And Snake 282205.py
@@ -0,0 +1,26 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n, m = map(int, input().split())
+printed = ['#' for i in range(m)]
+snake = [['.' for i in range(m)] for _ in range(n)]
+
+count = 0
+for i in range(n):
+    if i % 2 == 0:
+        snake[i] = printed
+        continue
+    
+    if count % 2 == 0:
+        snake[i][-1] = '#'
+        count += 1
+    else:
+        snake[i][0] = '#'
+        count += 1
+
+for s in snake:
+    print(''.join(s))
+
+
+
+
+    

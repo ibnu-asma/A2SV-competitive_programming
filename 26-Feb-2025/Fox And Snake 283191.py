e98023dc156f0376321834d181ae3c7eac9e5cc5
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-02-26 12:41:06 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 26-Feb-2025/Fox And Snake 283191.py
@@ -0,0 +1,29 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n , m = map(int, input().split())
+body = ['#'] * m
+placeholder = [['.' for _ in range(m)] for _ in range(n)]
+
+
+count = 0
+for i in range(n):
+    if i % 2 == 0:
+        placeholder[i] = body
+        continue
+    if count % 2 == 0:
+        placeholder[i][-1] = '#'
+        count += 1
+    else:
+        placeholder[i][0] = "#"
+        count += 1
+
+    
+[
+    ['#', '#', '#', '#'],
+     ['.', '.', '.', '#'],
+      ['#', '#', '#', '#']
+]
+
+for line in placeholder:
+    print(''.join(line))
+

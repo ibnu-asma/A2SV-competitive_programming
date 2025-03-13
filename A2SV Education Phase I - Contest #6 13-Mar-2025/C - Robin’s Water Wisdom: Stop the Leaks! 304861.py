4c8905fabc894f7af36e501f0a6b2da74fcbcf45
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-03-13 14:49:10 +0300

    Created 'C - Robin’s Water Wisdom: Stop the Leaks!' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 13-Mar-2025/C - Robin’s Water Wisdom: Stop the Leaks! 304861.py
@@ -0,0 +1,12 @@
+# Problem: C - Robin’s Water Wisdom: Stop the Leaks! - https://codeforces.com/gym/594356/problem/C
+
+n, given_volume, requried_volume = map(int, input().split())
+arr = list(map(int, input().split()))
+total_holes = sum(arr)
+res = sorted(arr[1:])
+count = 0
+while arr[0]/total_holes * given_volume < requried_volume:
+    total_holes -= res.pop()
+    count += 1
+
+print(count)

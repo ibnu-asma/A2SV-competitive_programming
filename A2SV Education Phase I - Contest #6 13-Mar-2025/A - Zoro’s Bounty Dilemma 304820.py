6ecb20cbe4dc33c99a7a2ee4f8780ea482686944
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-03-13 14:10:42 +0300

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 13-Mar-2025/A - Zoro’s Bounty Dilemma 304820.py
@@ -0,0 +1,14 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+n = int(input())
+for _ in range(n):
+    data = input()
+    if '<' in data and '>' in data:
+        print('?')
+    elif '<' in data:
+        print('<')
+    elif '>' in data:
+        print('>')
+    else:
+        print('=')
+        

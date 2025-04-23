e69354c3686b2db97a8e53db6e4c79b55131bab5
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-04-23 23:08:17 +0300

    Created 'Crawler Log Folder' by A2SV Hub

---
+++ 23-Apr-2025/Crawler Log Folder 339950.py
@@ -0,0 +1,16 @@
+# Problem: Crawler Log Folder - https://leetcode.com/problems/crawler-log-folder/
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        depth = 0
+        for log in logs:
+            if log == '../':
+                if depth > 0:
+                    depth -= 1
+            elif log == './':
+                pass
+            else:
+                depth += 1
+
+        
+        return depth

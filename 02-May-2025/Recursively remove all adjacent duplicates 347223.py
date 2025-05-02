87fcaf652b583e4cce626835a9c88d5c737fde3b
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-05-02 21:29:41 +0300

    Created 'Recursively remove all adjacent duplicates' by A2SV Hub

---
+++ 02-May-2025/Recursively remove all adjacent duplicates 347223.py
@@ -0,0 +1,22 @@
+# Problem: Recursively remove all adjacent duplicates - https://www.geeksforgeeks.org/recursively-remove-adjacent-duplicates-given-string/
+
+def rremove(s):
+    sb = ""
+    n = len(s)
+    i = 0
+    while i < n:
+        repeated = False
+
+        while i + 1 < n and s[i] == s[i + 1]:
+            repeated = True
+            i += 1
+
+        
+        if not repeated:
+            sb += s[i]
+        i += 1
+    if n == len(sb):
+        return sb
+   
+    return rremove(sb)
+

3c29e4f7dc624fef4f80a2cd0ac99a58b55bde12
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-08-20 10:13:54 +0300

    Created 'Sum of Nodes with Even-Valued Grandparent' by A2SV Hub

---
+++ 20-Aug-2025/Sum of Nodes with Even-Valued Grandparent 387110.py
@@ -0,0 +1,23 @@
+# Problem: Sum of Nodes with Even-Valued Grandparent - https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
+        def dfs(root,dad,gp):
+            if not root:
+                return 0
+            x=0
+            if(gp%2==0):
+                x=root.val
+            x+=dfs(root.left,root.val,dad)
+            x+=dfs(root.right,root.val,dad)
+            return x
+
+        return dfs(root,1,1)
+        
+        

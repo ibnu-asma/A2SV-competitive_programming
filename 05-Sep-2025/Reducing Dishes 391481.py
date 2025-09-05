e8dd853bde988f8e1f81cdf8bffb0be8a9cd5fca
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-09-05 22:16:08 +0300

    Created 'Reducing Dishes' by A2SV Hub

---
+++ 05-Sep-2025/Reducing Dishes 391481.py
@@ -0,0 +1,11 @@
+# Problem: Reducing Dishes - https://leetcode.com/problems/reducing-dishes/
+
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        res = total = 0
+        satisfaction.sort()
+        while satisfaction and satisfaction[-1] + total > 0:
+            total += satisfaction.pop()
+            res += total
+        return res
+        

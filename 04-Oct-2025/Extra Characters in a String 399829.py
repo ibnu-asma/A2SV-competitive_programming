ad3627b557e98c4b74caf47f13906c2e4a5d4ed7
Abdulhamid Hayredin <90865550+ibnu-asma@users.noreply.github.com>
2025-10-04 22:34:35 +0300

    Created 'Extra Characters in a String' by A2SV Hub

---
+++ 04-Oct-2025/Extra Characters in a String 399829.py
@@ -0,0 +1,15 @@
+# Problem: Extra Characters in a String - https://leetcode.com/problems/extra-characters-in-a-string/description/
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        dp = [0] * 51  
+        n = len(s)
+
+        for i in range(n - 1, -1, -1):
+            dp[i] = 1 + dp[i + 1] 
+            for w in dictionary:
+                if i + len(w) <= n and s[i:i + len(w)] == w:
+                    dp[i] = min(dp[i], dp[i + len(w)])  
+
+        return dp[0] 
+        
